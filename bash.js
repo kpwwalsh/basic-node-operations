3201f655ebef3d0147e5091b15ee11e74f093273
kpwwalsh <wnuru@hotmail.com>
2019-08-07 09:27:42 -0500

    basic node oper edit

--- bash.js
+++ bash.js
@@ -1,11 +1,8 @@
-const commands = require("./commands.js");
+const commands = require("./commands");
 
-//prompt the user for input
- process.stdout.write('prompt > ');
+process.stdout.write('prompt > ');
 
-// The stdin 'data' event triggers after a user types in a line
- process.stdin.on('data', (userInput) => {
+process.stdin.on('data', userInput => {
    userInput = userInput.toString().trim();
-  //evaluateCmd is a function which will be implemented in commands.js
    commands.evaluateCmd(userInput);
  });
--- commands.js
+++ commands.js
@@ -1,42 +1,37 @@
 const fs = require("fs");
 
-//write out data
- function done(output) {
-     process.stdout.write(output);
-     process.stdout.write('\nprompt > ');
+function done(output) {
+   process.stdout.write(output);
+   process.stdout.write('\nprompt > ');
  }
 
-//where we will store our commands
- function evaluateCmd(userInput) {
-  //parses the user input to understand which command was typed
+
+function evaluateCmd(userInput) {
    const userInputArray = userInput.split(" ");
    const command = userInputArray[0];
 
    switch (command) {
     case "echo":
-     //we will add the functionality of echo next within the object commandLibrary    
       commandLibrary.echo(userInputArray.slice(1).join(" "));
       break;
-      case "cat":
-        commandLibrary.cat(userInputArray.slice(1));
-        break;
-      case "head":
-        commandLibrary.head(userInputArray.slice(1).join(""));
-        break;
-      case "tail":
-        commandLibrary.head(userInputArray.slice(1).join(""));
-        break;
-      default:
-        commandLibrary.errorHandler();
+    case "cat":
+      commandLibrary.cat(userInputArray.slice(1));
+      break;
+    case "head":
+      commandLibrary.head(userInputArray.slice(1));
+      break;
+    case "tail":
+      commandLibrary.tail(userInputArray.slice(1));
+      break;
+    default:
+      commandLibrary.errorHandler();
   }
  }
 
-//where we will store the logic of our commands
  const commandLibrary = {
     "echo": function(userInput) {
         done(userInput);
     },
-    //the cat command
     "cat": function(fullPath) {
          const fileName = fullPath[0];
          fs.readFile(fileName, (err, data) => {
@@ -68,7 +63,7 @@ const fs = require("fs");
        },
      'errorHandler': () => {
         const error = "Incorrect command";
-        done(error);
+           done(error);
      }
  };
 
